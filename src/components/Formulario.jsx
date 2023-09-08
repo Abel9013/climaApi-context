23e4ed830f12252d45063c1b448c4e55fbbaf53a
Abel <90abel.avr@gmail.com>
2023-09-08 01:04:55 -0300

    added hook and request API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,52 @@
-
+import {useState} from "react"
+import useClima from "../hooks/useClima"
 const Formulario = () => {
+    
+    const [alerta, setAlerta] = useState('')
+    const {busqueda, datosBusqueda, consultarClima} = useClima()
+    const {ciudad, pais} = busqueda
+    
+    const handleSubmit = e => {
+      e.preventDefault()
+      if(Object.values(busqueda).includes('')){
+        setAlerta("Todos los campos son obligatorios")
+        return
+      }
+      consultarClima(busqueda)
+
+    } 
   return (
-    <div>Formulario</div>
+    <div className="contenedor">
+      {alert && <p>{alerta} </p>}
+      <form
+        onSubmit={handleSubmit}
+      >
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input type="text" id="ciudad" name="ciudad" onChange={datosBusqueda} value={ciudad} />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">País</label>
+          <select id="pais" name="pais" onChange={datosBusqueda} value={pais} >
+            <option value="">Seleccione un país</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">México</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+            <option value="BR">Brasil</option>
+            <option value="CL">Chile</option>
+            <option value="DK">Dinamarca</option>
+          </select>
+        </div>
+        <input
+          type="submit"
+          value="consultar clima"
+        />
+      </form>
+    </div>
   )
 }
 
